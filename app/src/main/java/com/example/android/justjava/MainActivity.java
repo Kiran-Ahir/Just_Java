7123d789144280502421aa8e6a7f3c687733944d
Kiran-Ahir <kiranj695@gmail.com>
2021-05-29 20:04:33 +0530

    Final Version by Udacity

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
-    int price = 5;
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,32 +24,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        String priceMessage = "Total: $"+(quantity*price)+"\nThank you!";
-        displayMessage(priceMessage);
+        EditText text = (EditText) findViewById(R.id.name_field);
+        String name = text.getText().toString();
+
+        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckbox.isChecked();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.intent_subject, name));
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int basePrice = 5;
+
+        if(addWhippedCream){
+            basePrice += 1;
+        }
+
+        if(addChocolate){
+            basePrice += 2;
+        }
+
+        return quantity*basePrice;
     }
 
-    private void display(int number){
+    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage = getString(R.string.order_summary_name, name);
+        priceMessage += getString(R.string.order_summary_add_cream, addWhippedCream);
+        priceMessage += getString(R.string.order_summary_add_chocolate, addChocolate);
+        priceMessage += getString(R.string.order_summary_quantity, quantity);
+        priceMessage += getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(price));
+        priceMessage += getString(R.string.order_summary_thank_you);
+        return priceMessage;
+    }
+
+    private void displayQuantity(int number){
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText(""+number);
     }
 
-    private void displayPrice(int number){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
     public void increment(View view) {
+        if(quantity==100){
+            Toast.makeText(this, getString(R.string.not_more_than_hundred_coffee), Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
+        if(quantity==1){
+            Toast.makeText(this, getString(R.string.not_less_than_one_coffee), Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity--;
-        display(quantity);
+        displayQuantity(quantity);
     }
 }
